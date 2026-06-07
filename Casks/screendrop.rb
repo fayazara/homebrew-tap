ff08d352244349c5e6d307b120818d7f84a1aded
fayaz <faralikatti@cloudflare.com>
2026-06-07 16:26:16 +0530

    screendrop 0.20.3

--- Casks/screendrop.rb
+++ Casks/screendrop.rb
@@ -1,6 +1,6 @@
 cask "screendrop" do
-  version "0.20.2"
-  sha256 "54657d2b3d31a1fe122a86b2869eb06dd3457b6ac66b79c86a110397403ecf24"
+  version "0.20.3"
+  sha256 "b28d9569776db439275ed90d48886584375e2ef89819c8c6bb4cc530567b4fbf"
 
   url "https://github.com/fayazara/screendrop/releases/download/v#{version}/Screendrop.dmg"
   name "Screendrop"
